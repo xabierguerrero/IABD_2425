d257e96963a0db09293f6a9c272296894187cffa
xabierguerrero <xabierguerrero@gmail.com>
2024-10-28 19:11:11 +0100

    hay que dividir datos en titanic

---
+++ Python_Ejers/SBIG-BIGD/SGBD_2_3_Ejer_11.py
@@ -0,0 +1,10 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import datetime as dt
+import seaborn as sns
+import networkx as nx
+import pandas as pd
+
+data=[9, 12, 6, 11, 19, 5, 8, 13, 2, 8, 5, 12, 0, 9, 4, 15, 18, 10, 6, 16]
+data.sort()
+print(data)
