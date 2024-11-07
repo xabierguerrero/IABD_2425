2b56021ca7091f1d51b3d79b3bd9fb2dbdc024c0
xabierguerrero <xabierguerrero@gmail.com>
2024-11-07 16:38:51 +0100

    SBGD 2_4 acabdo

--- Python_Ejers/SBIG-BIGD/SBGD_2_4_Ejer2.py
+++ Python_Ejers/SBIG-BIGD/SBGD_2_4_Ejer2.py
@@ -5,3 +5,9 @@ import numpy as np
 
 avg=3.2
 std=0.5
+
+prob3_5kg=round(st.norm.cdf(3.505,avg,std)-st.norm.cdf(3.495,avg,std),3)
+peso_80_mas_pesado=round(st.norm.ppf(0.8, avg, std),3) 
+
+print("Probabilidad peso recien nacido =3.5:",prob3_5kg)
+print("Peso minimo del 20% de los bebes mas pesados:",peso_80_mas_pesado)
